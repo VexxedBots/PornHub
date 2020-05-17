757ddaec05f231feb5427e84315b23453fa9a32d
VexxedBots <63519260+VexxedBots@users.noreply.github.com>
2020-05-18 00:57:19 +0200

    Update karbonlund.py

--- stdplugins/karbonlund.py
+++ stdplugins/karbonlund.py
@@ -67,7 +67,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="@R4V4N4 SAYS: Kya Comment Dakun Behenchod",
+         caption="Done",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
