0560aad97bcdb07e18ee77d6c98e88b687e96721
VexxedBots <63519260+VexxedBots@users.noreply.github.com>
2020-05-17 22:25:38 +0200

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -66,81 +66,46 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == borg.uid and query.startswith("@UniBorg"):
+        if event.query.user_id == borg.uid and query.startswith("@Bot"):
             rev_text = query[::-1]
             buttons = paginate_help(0, borg._plugins, "info")
             result = builder.article(
-                "© @UniBorg",
+                "© @Bot",
                 text="{}\nℂ𝕦𝕣𝕣𝕖𝕟𝕥𝕝𝕪 𝕃𝕠𝕒𝕕𝕖𝕕 ℙ𝕝𝕦𝕘𝕚𝕟𝕤: {}".format(
                     query, len(borg._plugins)),
                 buttons=buttons,
                 link_preview=True
             )
-        elif query.startswith("choot"):
+        elif query.startswith("apksappschat"):
             result = builder.article(
-                "@r4v4n4: Bhagwaan Sabko GF De",
-                text=f"[Choot](https://telegra.ph/file/019a2eab3d66d39c92a75.mp4)",
+                "Follow This Group",
+                text=f"[Here](t.me/apksappschat)",
                 buttons=[],
-                link_preview=True
+                link_preview=False
             )
-        elif query.startswith("bhoot"):
+        elif query.startswith("moderse"):
             result = builder.article(
-                "@r4v4n4: Bhagwaan Sabko GF De",
-                text=f"[bhoot](https://da.gd/ovpt5)",
+                "Follow @Moderse",
+                text=f"[Here](t.me/moderse)",
                 buttons=[],
-                link_preview=True
+                link_preview=False
             )
-        elif query.startswith("repo"):
-            result = builder.article(
-                "@r4v4n4: yeh hai button deploy kar le",
-                text=f"Ganja Sutta on the floor",
-                buttons=[
-                    [custom.Button.url("👤Click on the button to deploy pornhub repo👤", "https://github.com/ravana69/Pornhub")],
-                ],
-                link_preview=True
-            )
-        elif query.startswith("imdb"):
-            result = builder.article(
-                "@r4v4n4: IMDB",
-                text="""**Title:** Choot Ki Raani (1969)
-**Rating ⭐️:** 10 / 10
-(5.5 based on 7,610 user ratings) | U | 0h 69min |
-**Release Info:** 14 Feb 1969 (India)
-**Genre:** 🌋 #Adventure 🤣 #Comedy #Family
-**Language:**  #Bhojpuri #English
-**Country of Origin:**  #India
-**Story Line:** A young man Ravana moves from Bihar to Florida with his dick, where he's compelled to engage in a sux to protect a population of endangered Choots.
-**Director:** Ravana @r4v4n4
-**Writers:** Ravana @r4v4n4
-**Stars:**  Ravana @r4v4n4
-[Read More ...](https://da.gd/qGtPI)""",
-                buttons=[
-                    [custom.Button.url("Open On IMDB ▶️", "https://da.gd/o5Yy")]
-                ],
-                link_preview=True
-                )
+        
         else:
             result = builder.article(
-                "© @UniBorg",
-                text="""@r4v4n4 **( Custom Built By** @r4v4n4 **)** 
+                "© Just Another Userbot",
+                text="""@iclapcheeks userbot 
 **Verified Account:** ✅
-**Official Website:** https://ravanaisdrunk.site.live [⠀](https://telegra.ph/file/b0604ea53360cd3858ec5.mp4)
 
-**Pithun 3.8.2 (default, Feb 27 2020, 21:41:26)** 
+**Python 3.8.2 (May 03 2020, 21:41:26)** 
 **[GCC 7.4.0]**
-**Talethrun 1.11.3**
-
-**Custom Built Fork:** https://github.com/ravana69/Pornhub""",
+**Telethon 1.11.3**""",
                 buttons=[
-                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/r4v4n4"), custom.Button.url(
-                        "📼Ravana Audio Memes📼", "https://t.me/tgaudiomemes")],
-                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/ravana69/Pornhub"), custom.Button.url(
-                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F&template=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F")],
-                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
-                        "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
-                        "📤Cloud Torrent📥", "https://github.com/ravana69/cloudtorrent")]
+                    [custom.Button.url("Moderse", "https://t.me/moderse"), custom.Button.url(
+                        "Android Apps Chat", "https://t.me/apksappschat")],
+                        [custom.Button.url("🔰Spamverse🔰", "https://bit.ly/3aUo5kq")]
                 ],
-                link_preview=True
+                link_preview=False
             )
         await event.answer([result] if result else None)
 
@@ -157,7 +122,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "⚠️ Warning: Don't Press Any Buttons ⚠️\n\nCustom Fork: https://github.com/ravana69/Pornhub\n\n\nNote: Bas kar BetiChod, Maa Ke Laude, Madarchod"
+            reply_pop_up_alert = "⚠️ Warning: Don't Press Any Buttons ⚠️"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -175,7 +140,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own @UniBorg, and don't edit my messages!"
+            reply_pop_up_alert = "Sigh"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -189,7 +154,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         reply_pop_up_alert = help_string if help_string is not None else \
             "No DOCSTRING has been setup for {} plugin".format(plugin_name)
         reply_pop_up_alert += "\n\n Use .unload {} to remove this plugin\n\
-            © @r4v4n4".format(plugin_name)
+            ©".format(plugin_name)
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
